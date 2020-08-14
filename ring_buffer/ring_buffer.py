2a1f0aafbf1f74d13b8f0918b84e6eb4baae8ec6
hera <a.heraimenka@gmail.com>
2020-08-14 08:40:12 -0700

    Create ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,22 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
-
-    def append(self, item):
-        pass
+        self.storage = []
+        self.capacity = capacity
+        self.index = 0
+    
+    def __str__(self):
+        return str(self.storage)
 
     def get(self):
-        pass
+        return self.storage
+    
+    def append(self, value):
+        if self.index >= self.capacity:
+            self.index = 0
+        
+        if len(self.storage) < self.capacity:
+            self.storage.append(value)
+        else:
+            self.storage[self.index] = value
+
+        self.index += 1
